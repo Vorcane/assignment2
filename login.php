82beb253c434bed2c2f4aedc75c4d735ec985303
Lachlan Blackhall <vorcane@gmail.com>
2016-10-18 00:49:49 +0800

    Added the login script
    Forgot to add it in when initialising

---
+++ login.php
@@ -0,0 +1,39 @@
+<?php 
+	$host = "localhost";
+	$user = "X32674186";
+	$password = "X32674186";
+	$dbname = "X32674186";
+	// Create Connection
+	$dbc = mysql_pconnect($host, $user, $password);
+	// Check connection 
+	if ($conn->connect_error) {
+		die("Connection Failed: " . $conn->connect_error);
+	}
+	// Select database
+	mysql_select_db($dbname);
+	// Get data from form
+	$enteredname = $_POST["username"];
+	$enteredpass = $_POST["password"];
+	// Search database with form data
+	$searchquery = "SELECT * FROM users WHERE userName = '" . $enteredname . "' AND password = '" . $enteredpass . "';";
+	$queryresult = mysql_query($searchquery);
+	// Check if data matches up
+	if(mysql_num_rows($queryresult)){
+		// Valid credentials 
+		$sessionid = rand(11111111111111111111, 99999999999999999999);
+		while($row=mysql_fetch_array($queryresult)){
+			//Process $row 
+		}
+		//Updates the session ID field into the database for the user
+		$insertinto = "UPDATE users SET sessionID = " . $sessionid . " WHERE userID = " . $row['userID'] . ";";
+		$queryresult = mysql_query($insertinto);
+		if(queryresult){ //Sets the cookie 
+			$cookiename = "sessionID";
+			$cookievalue = $sessionid;
+			setcookie($cookiename, $cookievalue, time()+ (86400 * 7), "/");
+		} else {
+		}
+	} ELSE {
+		//Invalid credentials
+	}
+?>
