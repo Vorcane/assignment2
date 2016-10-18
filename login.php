91024713a2a5090a9317e2bd3dd99d2bc3647e66
Lachlan Blackhall <vorcane@gmail.com>
2016-10-18 20:55:29 +0800

    Commented out debugging code
    It finally updates the database, how nice

--- login.php
+++ login.php
@@ -11,19 +11,19 @@
 	$dbc = mysql_pconnect($host, $user, $password) or die("Can't connect" . mysql_error());
 	// Select database
 	mysql_select_db($dbname) or die("Couldn't select DB" . mysql_error());
-    echo "test one <br>";
+    //echo "test one <br>"; //Debugging code, pray you dont need this
 
 	// Get data from form
 	$enteredname = $_POST["username"];
 	$enteredpass = $_POST["password"];
-    echo "test two <br>";
+    //echo "test two <br>"; //Debugging code, pray you dont need this
 
 	// Search database with form data
 	$searchquery = 'SELECT userID FROM users WHERE userName = "' . $enteredname . '" AND password = "' . $enteredpass . '";';
 	$queryresult = mysql_query($searchquery);
-    echo "test three <br>";
+    //echo "test three <br>"; //Debugging code, pray you dont need this
     $testvar = mysql_num_rows($queryresult);
-    echo "$testvar ayy lmao<br>";
+    //echo "$testvar ayy lmao<br>"; //Debugging code, pray you dont need this
 	// Check if data matches up
 	if(mysql_num_rows($queryresult))
         { 
@@ -31,14 +31,10 @@
             $sessionid = uniqid ('', true); //Does do this
             while($row=mysql_fetch_array($queryresult)){
                 //Process $row 
-                echo $row["userID"] . "This is a while loop <br>";
+                //echo $row["userID"] . "This is a while loop <br>"; //Debugging code, pray you dont need this
                 $insertinto = 'UPDATE users SET sessionID = "' . $sessionid . '" WHERE userID = "' . $row['userID'] . '";';
 		        $updateresult = mysql_query($insertinto);
             }
-       // echo $row["userID"] .  "xDDD<br>"; //This works now, no it doesnt the php LIES
-        
-		//Updates the session ID field into the database for the user
-        //mysql_free_result($queryresult); //Clears leftover data from select query DO I EVEN NEED THIS
         
         echo $updateresult . "This should be the query result";
 		
@@ -47,7 +43,7 @@
 			$cookiename = "sessionID";
 			$cookievalue = $sessionid;
 			setcookie($cookiename, $cookievalue, time()+ (86400 * 7), "/"); // Sets cookie with the id for expiry in a week
-			echo $cookievalue; //Gets here
+			//echo $cookievalue; //Gets here //Debugging code, pray you dont need this
             
 		} else {
             //Couldnt insert
