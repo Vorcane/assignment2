0e6953755f4f766c60e64c7c878e616a39aabe43
Lachlan Blackhall <vorcane@gmail.com>
2016-10-18 15:35:36 +0800

    Removed duplicate function and commented out non completed login function

--- javascript/login.js
+++ javascript/login.js
@@ -9,19 +9,9 @@ function validateForm(){
 	}
 }
 
-function login(){
+/*function login(){
 	var xhttp = new XMLHttpRequest();
 	xhttp.open("GET", "login.php", true);
 	xhttp.setRequestHeader("
 	xhttp.send();
-		
-}function validateForm(){
-	var usernamefield = document.getElementById("usernamefield").value;
-	var passwordfield = document.getElementById("passwordfield").value;
-	if(!usernamefield OR !passwordfield){
-		// Do something here
-		return false;
-	} else {
-		return true;
-	}
-}
+}*/
