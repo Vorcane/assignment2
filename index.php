e661b3d6a36efce9cb60ffc15cb1754f6d20604e
Lachlan Blackhall <vorcane@gmail.com>
2016-10-18 21:57:47 +0800

    test was a failure

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<link rel="stylesheet" type="text/css" href="./css/styles.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="javascript/login.js"></script>
-    <script src="javascript/test.js"></script>
+    <!--<script src="javascript/test.js"></script>-->
 
 </head>
 
@@ -40,11 +40,11 @@
 		
 		<!-- Login system starts here -->
 			<div id="form-messages"></div>
-			<!--<form action="login.php" method="post" onsubmit=" return validateForm()">-->
+			<form action="login()" method="post" onsubmit=" return validateForm()">
                 Username: <input type="text" name="username" id="usernamefield">
                 Password: <input type="password" name="password" id="passwordfield">
                 <input type="submit" value="Login" name="Login" id="login-submit">
-			<!--</form>-->
+			</form>
 		<!-- Login system ends here -->
 		
 		<!--Code for the section that will contain the products-->
