e2d1fef0c7aa9ba0be9338101f5de9252059fb68
Lachlan Blackhall <vorcane@gmail.com>
2016-10-20 18:13:34 +0800

    php ajax test one

--- javascript/login.js
+++ javascript/login.js
@@ -1,5 +1,12 @@
 $('input#login-submit').on('click', function() {
     var username = $('input#username').val();
+    var password = $('input#password').val();
+    if ($.trim(username) != '' && $.trim(password) != '') {
+        $.post('login.php', {username: username, password: password}, function(data) {
+            $('div#login-data).text(data);
+        })
+    }
+    
     alert(username);
 })
 
