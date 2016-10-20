f1d847151928e9bfd8f09fa227118860b7caae37
Lachlan Blackhall <vorcane@gmail.com>
2016-10-20 19:30:02 +0800

    php test 3

--- javascript/login.js
+++ javascript/login.js
@@ -1,7 +1,8 @@
 $('input#login-submit').on('click', function() {
     var username = $('input#username').val();
     var password = $('input#password').val();
-    if ($.trim(username) != '' && $.trim(password) != '') {
+    alert(password);
+    if (($.trim(username) != '') && ($.trim(password) != '')) {
         $.post('login.php', {username: username, password: password}, function(data) {
             $('div#login-data).text(data);
             alert(password);
