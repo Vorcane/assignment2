4fd8276571ee288a37bc4029b2e215a08a9d25ca
Lachlan Blackhall <vorcane@gmail.com>
2016-10-21 17:20:00 +0800

    Removed debug lines and added fail conditions for login.php

--- javascript/login.js
+++ javascript/login.js
@@ -7,30 +7,4 @@ $('input#login-submit').on('click', function() {
         })
     }
 })
-
-
-//function validateForm() {
-//	var usernamefield = document.getElementById("usernamefield").value;
-//	var passwordfield = document.getElementById("passwordfield").value;
-//	if(!usernamefield || !passwordfield){
-//		// Do something here
-//		return false;
-//	} else {
-//		return true;
-//	}
-//}
-//
-//function loginHandler(){
-//	/*var xhttp = new XMLHttpRequest();
-//	xhttp.open("GET", "login.php", true);
-//	xhttp.setRequestHeader("
-//	xhttp.send();*/
-//    
-//    var username = $('input#usernamefield').val();
-//    var password = $('input#passwordfield').val();
-//    alert(name);
-//}
-//
-//}
-
     
--- login.php
+++ login.php
@@ -8,46 +8,37 @@
 	$dbc = mysql_pconnect($host, $user, $password) or die("Can't connect" . mysql_error());
 	// Select database
 	mysql_select_db($dbname) or die("Couldn't select DB" . mysql_error());
-    //echo "test one <br>"; //Debugging code, pray you dont need this
 
 	// Get data from form
-	$enteredname = $_POST["username"];
-	$enteredpass = $_POST["password"];
-    //echo "test two <br>"; //Debugging code, pray you dont need this
+	$enteredname = trim($_POST["username"]);
+	$enteredpass = trim($_POST["password"]);
 
 	// Search database with form data
-	$searchquery = 'SELECT userID FROM users WHERE userName = "' . $enteredname . '" AND password = "' . $enteredpass . '";';
+	$searchquery = 'SELECT userID FROM users WHERE userName = "' . mysql_real_escape_string($enteredname) . '" AND password = "' . mysql_real_escape_string($enteredpass) . '";';
 	$queryresult = mysql_query($searchquery);
-    //echo "test three <br>"; //Debugging code, pray you dont need this
-    $testvar = mysql_num_rows($queryresult);
-    //echo "$testvar ayy lmao<br>"; //Debugging code, pray you dont need this
 	// Check if data matches up
-	if(mysql_num_rows($queryresult))
+	if(mysql_num_rows($queryresult) !== 0)
         { 
             // Valid credentials 
             $sessionid = uniqid ('', true); //Does do this
             while($row=mysql_fetch_array($queryresult)){
                 //Process $row 
-                //echo $row["userID"] . "This is a while loop <br>"; //Debugging code, pray you dont need this
                 $insertinto = 'UPDATE users SET sessionID = "' . $sessionid . '" WHERE userID = "' . $row['userID'] . '";';
 		        $updateresult = mysql_query($insertinto);
-            }
-        
-        //echo $updateresult . "This should be the query result"; //Debugging code, pray you dont need this
+                if($updateresult)
+                    { //Sets the cookie 
+                        $cookiename = "sessionID";
+                        $cookievalue = $sessionid;
+                        setcookie($cookiename, $cookievalue, time()+ (86400 * 7), "/"); // Sets cookie with the id for expiry in a week
+                        echo "Your cookie is: $cookievalue";
+
+                    } else {
+                        echo "Login failed, please try again";
+                    }
+        }
 		
-        if($updateresult)
-        { //Sets the cookie 
-			$cookiename = "sessionID";
-			$cookievalue = $sessionid;
-			setcookie($cookiename, $cookievalue, time()+ (86400 * 7), "/"); // Sets cookie with the id for expiry in a week
-			//echo $cookievalue; //Gets here //Debugging code, pray you dont need this
-            echo "Your cookie is: $cookievalue";
-            
-		} else {
-            //Couldnt insert
-		}
-	} ELSE {
-		//Invalid credentials
+	} else {
+		echo "Invalid username or password";
 	}
     mysql_close(); //Ends DB connection
 ?>
