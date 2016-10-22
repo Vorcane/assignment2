cb5cd3224c5e3c33559ad7090c7e6e947db41d70
Lachlan Blackhall <vorcane@gmail.com>
2016-10-22 19:15:32 +0800

    More syntaxing

--- javascript/loginscripts.js
+++ javascript/loginscripts.js
@@ -1,4 +1,5 @@
-
+/*global $, checklogin*/
+/*jslint plusplus: true*/
 function login() {
     "use strict";
     $('input#login-submit').on('click', function () {
@@ -17,37 +18,34 @@ function login() {
 function checklogin() {
     "use strict";
     var cookiename = "sessionID=",
-        cookiearray = document.cookie.split(';'); //Splits the cookie into a sub array
+        cookiearray = document.cookie.split(';'), //Splits the cookie into a sub array
+        i = 0,
+        sessionID,
+        cookie,
+        retdata;
     $(document).ready(function () {
-        for (var i = 0; i <cookiearray.length; i++) {
-            var cookie = cookiearray[i]; //Gets the individual cookie from the array
-            while (cookie.charAt(0)==' ') { //Clears blank space from the value
+        for (i = 0; i < cookiearray.length; i++) {
+            cookie = cookiearray[i]; //Gets the individual cookie from the array
+            while (cookie.charAt(0) === ' ') { //Clears blank space from the value
                 cookie = cookie.substring(1);
             }
-            if (cookie.indexOf(cookiename) == 0) { //If cookie exists do this
+            if (cookie.indexOf(cookiename) === 0) { //If cookie exists do this
                 //sends only value not the name of the cookie
                 //$('div#login-form').text("I got into the if statement"); // Gets here
-                var sessionID = cookie.substring(name.length+10,cookie.length);
-                $.post('checklogin.php', {sessionID: sessionID}, function(data) { 
+                sessionID = cookie.substring(name.length + 10, cookie.length);
+                $.post('checklogin.php', {sessionID: sessionID}, function (data) {
                     retdata = data.split(';');
-                    if (retdata[0]==true) {
-                        $('div#login-form').html(
-                            "<h3>Welcome " + retdata[1] + "</h3>"
-                            + "<button onclick='deletecookie(\x22sessionID\x22)' type='button'>Log out</button>"
-
-
-                        ); //removes the login forms and replaces it with a greeting message, logout button and link to user account page
+                    if (retdata[0] === true) {
+                        $('div#login-form').html("<h3>Welcome " + retdata[1] + "</h3>" + "<button onclick='deletecookie(\x22sessionID\x22)' type='button'>Log out</button>"); //removes the login forms and replaces it with a greeting message, logout button and link to user account page
                     }
-                })
+                });
 
-            } else { //If cookie doesnt exist do this\
-
-                } 
+            }
         }
     });
 }
 
 function deletecookie(name) {
-    var d = new Date();
-    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';    
+    "use strict";
+    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
