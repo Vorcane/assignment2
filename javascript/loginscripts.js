7f6a9f7a634006e4a4a590af6201f169e70e3338
Lachlan Blackhall <vorcane@gmail.com>
2016-10-22 21:20:52 +0800

    test

--- javascript/loginscripts.js
+++ javascript/loginscripts.js
@@ -12,8 +12,7 @@ function checklogin() {
         cookiearray = document.cookie.split(';'), //Splits the cookie into a sub array
         i = 0,
         sessionID,
-        cookie,
-        retdata;
+        cookie;
     $('div#login-data').text("I get inside the check login function");
     $(document).ready(function () {
         $('div#login-data').text("I get inside the document ready method");
@@ -24,10 +23,10 @@ function checklogin() {
             }
             if (cookie.indexOf(cookiename) === 0) { //If cookie exists do this
                 //sends only value not the name of the cookie
-                $('div#login-form').text("I got into the if statement"); // Gets here
+                //$('div#login-form').text("I got into the if statement"); // Gets here
                 sessionID = cookie.substring(name.length + 10, cookie.length);
                 $.post('checklogin.php', {sessionID: sessionID}, function (data) {
-                    retdata = data.split(';');
+                    var retdata = data.split(';');
                     if (retdata[0] === true) {
                         $('div#login-form').html("<h3>Welcome " + retdata[1] + "</h3>" + "<button onclick='deletecookie(\x22sessionID\x22)' type='button'>Log out</button>"); //removes the login forms and replaces it with a greeting message, logout button and link to user account page
                     }
