5793c128061c557b6949e947010687c96fa5efce
Lachlan Blackhall <vorcane@gmail.com>
2016-10-23 00:03:12 +0800

    log out test

--- javascript/loginscripts.js
+++ javascript/loginscripts.js
@@ -3,7 +3,7 @@
 
 function deletecookie(name) {
     "use strict";
-    document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    Cookies.remove("'"+name+"'");
 }
 
 function checklogin() {
