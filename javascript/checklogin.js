e6767e1ce22098ccfba9f006eb6d0fb5ad00600e
Lachlan Blackhall <vorcane@gmail.com>
2016-10-22 13:16:28 +0800

    js edit test

--- javascript/checklogin.js
+++ javascript/checklogin.js
@@ -8,7 +8,8 @@ for(var i = 0; i <cookiearray.length; i++) {
     if (cookie.indexOf(cookiename) == 0) { //If cookie exists do this
         //sends only value not the name of the cookie
         //$('div#login-form').text("I got into the if statement"); // Gets here
-        $.post('checklogin.php', {sessionID: cookie.substring(name.length+10,cookie.length)}, function(data) { 
+        var sessionID = cookie.substring(name.length+10,cookie.length);
+        $.post('checklogin.php', {sessionID: sessionID}, function(data) { 
             retdata = data.split(';');
             if (retdata[0]==true) {
                 $('div#login-form').text("<h3>Welcome " + retdata[1] + "</h3>"); //removes the login forms and replaces it with a greeting message, logout button and link to user account page
