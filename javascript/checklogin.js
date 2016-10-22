64b85c72823052fa2797278a43b733a12e948105
Lachlan Blackhall <vorcane@gmail.com>
2016-10-22 11:50:21 +0800

    Compensated for missing jquery by rewriting cookie function

--- javascript/checklogin.js
+++ javascript/checklogin.js
@@ -1,5 +1,14 @@
-if (!!$.cookie('sessionID')) {
-    // No cookie do nothing
-} else {
-    document.write("Cookie found:" + $.cookie('sessionID'));
+var cookiename = "sessionID=";
+var cookiearray = document.cookie.split(';'); //Splits the cookie into a sub array
+for(var i = 0; i <cookiearray.length; i++) {
+    var cookie = cookiearray[i]; //Gets the individual cookie from the array
+    while (cookie.charAt(0)==' ') { //Clears blank space from the value
+        cookie = cookie.substring(1);
+    }
+    if (c.indexOf(cookiename) == 0) { //If cookie exists do this
+        document.write(c.substring(name.length,c.length)); //Returns the cookie value if found
+        
+    } else { //If cookie doesnt exist do this
+        document.write("Cookie not found")
+    }
 }
