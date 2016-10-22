0b47d113b92e888fa4a5d446a8857afabd9a1071
Lachlan Blackhall <vorcane@gmail.com>
2016-10-22 23:31:09 +0800

    restore that debug code

--- javascript/loginscripts.js
+++ javascript/loginscripts.js
@@ -12,7 +12,7 @@ function checklogin() {
     
     $.post('checklogin.php', {sessionID: sessionID}, function (data) {
                     var retdata = data.split(';');
-                    //$('div#login-form').html(retdata[0]);
+                    $('div#login-form').html(retdata[0]);
                     if (retdata[0] === true) {
                         $('div#login-form').html("<h3>Welcome " + retdata[1] + "</h3>" + "<button onclick='deletecookie(\x22sessionID\x22)' type='button'>Log out</button>"); //removes the login forms and replaces it with a greeting message, logout button and link to user account page
                     }
