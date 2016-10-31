5d568fe86f88d7831d0a1de09b9648e0855d7b7c
Lachlan Blackhall <vorcane@gmail.com>
2016-11-01 01:07:03 +0800

    test

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -51,6 +51,7 @@ function login() {
     if (($.trim(username) !== '') && ($.trim(password) !== '')) {
         $.post('ajax/login.php', {username: username, password: password}, function (data) {
             found = data;
+            document.write(data);
         });
         if (found === 1) {
             location.reload();
