91bb362a5b9dcd3e301bd1e2a656f1d5cda99c87
Lachlan Blackhall <vorcane@gmail.com>
2016-11-01 01:37:42 +0800

    replaced " with '

--- index.php
+++ index.php
@@ -65,9 +65,9 @@
                     }
 
                 
-                    echo "<form class="search" name="searchBar" action="javascript:AnyFunction();" method="POST">"
-                    echo "    <label>Search: <input type="text" id="search" name="searchbar" placeholder="$placeholdertext"></input> </label>"
-                    echo "    <input type="submit" id="search-submit" value="search"></input>"
+                    echo "<form class='search' name='searchBar' action='javascript:AnyFunction();' method='POST'>"
+                    echo "    <label>Search: <input type='text' id='search' name='searchbar' placeholder='$placeholdertext'></input> </label>"
+                    echo "    <input type='submit' id='search-submit' value='search'></input>"
                     echo "</form>"
                 ?>
 			</ul>
