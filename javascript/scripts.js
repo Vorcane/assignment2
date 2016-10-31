6c1d871250ec77e0490ca6b5f9e332873654a354
Lachlan Blackhall <vorcane@gmail.com>
2016-11-01 01:03:57 +0800

    test

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -4,14 +4,16 @@
 
 function loadcart() {
     "use strict";
-    var currentcart = Cookies.get('cart');
-    document.write(currentcart);
-        //splitcart = currentcart.split('*');
-//    splitcart.forEach(function (productID) {
-//        $.post('ajax/cart.php', {productID: productID}, function (data) {
-//            $('div#cart').append('<li><a href="#">' + data + "</a></li>");
-//        });
-    //});
+    var currentcart = Cookies.get('cart'),
+        splitcart;
+    if (currentcart) {
+        splitcart = currentcart.split("*");
+        splitcart.forEach(function (productID) {
+            $.post('ajax/cart.php', {productID: productID}, function (data) {
+                $('div#cart').append('<li><a href="#">' + data + "</a></li>");
+            });
+        });
+    }
 }
 
 function deletecookie() {
