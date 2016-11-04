2ca6b9d04bb8de00e3a549b6394e30546a68d15c
Lachlan Blackhall <vorcane@gmail.com>
2016-11-04 16:52:42 +0800

    test

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -71,7 +71,7 @@ function checklogin() {
 function deletecookie() {
     "use strict";
     Cookies.remove('sessionID');
-    location.href = location.href;
+    location = 'http://ceto.murdoch.edu.au/~32667253/assignment2/#';
 }
 
 function login() {
@@ -84,7 +84,7 @@ function login() {
         $.post('ajax/login.php', {username: username, password: password}, function (data) {
             found = data;
             if (found === "1") { // Only reload on valid credentials
-                location.href = location.href;
+                location = 'http://ceto.murdoch.edu.au/~32667253/assignment2/#';
             }
         });
     }
@@ -119,7 +119,7 @@ function edituserdetails() {
             var retdata = data.split(';');
             if (retdata[0] === "1" && retdata[1] === oldpassword) {
                 $.post('ajax/updateuserdetails.php', {username: username, password: newpassword, name: name, contactnumber: contactnumber, address: address, email: email, sessionID: sessionID}, function (data) {
-                    location.href = location.href;
+                    location = 'http://ceto.murdoch.edu.au/~32667253/assignment2/#';
                 });
             }
         });
@@ -153,7 +153,7 @@ function addproducts() {
         tags = $('input#tags').val();
         
         $.post('ajax/addproduct.php', {name: name, price: price, description: description, genre: genre, quantity: quantity, rating: rating, image: image, tags: tags}, function (data) {
-            location.href = location.href;
+            location = 'http://ceto.murdoch.edu.au/~32667253/assignment2/#';
         });
     });
 }
@@ -171,7 +171,7 @@ function addtocart(productID) {
 function clearcart() {
     "use strict";
     Cookies.set('cart', '');
-    location.href = location.href;
+    location = 'http://ceto.murdoch.edu.au/~32667253/assignment2/#';
 //    var outputhtml = '<li><a href="#" onclick="clearcart()">Clear cart</a></li>';
 //    $('div#cart').html = outputhtml;
 }
