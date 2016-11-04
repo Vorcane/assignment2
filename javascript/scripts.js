97af61d9f72314ffd3a550800f1f887f7d2d9f63
Lachlan Blackhall <vorcane@gmail.com>
2016-11-04 17:36:05 +0800

    test

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -83,7 +83,7 @@ function checklogin() {
 function deletecookie() {
     "use strict";
     Cookies.remove('sessionID');
-    window.location = window.location;
+    location.reload();
 }
 
 function login() {
@@ -96,7 +96,7 @@ function login() {
         $.post('ajax/login.php', {username: username, password: password}, function (data) {
             found = data;
             if (found === "1") { // Only reload on valid credentials
-                window.location=window.location;
+                location.reload();
             }
         });
     }
@@ -131,7 +131,7 @@ function edituserdetails() {
             var retdata = data.split(';');
             if (retdata[0] === "1" && retdata[1] === oldpassword) {
                 $.post('ajax/updateuserdetails.php', {username: username, password: newpassword, name: name, contactnumber: contactnumber, address: address, email: email, sessionID: sessionID}, function (data) {
-                    window.location=window.location;
+                    location.reload(true);
                 });
             }
         });
@@ -165,7 +165,7 @@ function addproducts() {
         tags = $('input#tags').val();
         
         $.post('ajax/addproduct.php', {name: name, price: price, description: description, genre: genre, quantity: quantity, rating: rating, image: image, tags: tags}, function (data) {
-            window.location=window.location;
+            location.reload(true);
         });
     });
 }
@@ -183,7 +183,7 @@ function addtocart(productID) {
 function clearcart() {
     "use strict";
     Cookies.set('cart', '');
-    window.location=window.location;
+    location.reload(true);
 //    var outputhtml = '<li><a href="#" onclick="clearcart()">Clear cart</a></li>';
 //    $('div#cart').html = outputhtml;
 }
