1713d0a3d9e477acd21f4cdf2fa198368b99e339
Lachlan Blackhall <vorcane@gmail.com>
2016-11-04 17:10:11 +0800

    test

--- ajax/product.php
+++ ajax/product.php
@@ -9,12 +9,13 @@
 	
 	# Displaying what the query retrieved
 	while ($row = mysql_fetch_assoc($query)) {
+        $productID = $row["productID"];
 		echo "<div class='prodContainer'>"; # Starting the container
 		echo "<p>" . $row["name"] . "</p>";
 		echo "<p>$" . $row["price"] . "</p>";
 		echo "<p>" . $row["description"] . "</p>";
 		echo "<p>" . $row["rating"] . "</p>";
-		echo "<form name='addButton' action='' method='POST'><input type='submit' name='addToC' value='Add to Cart'></input></form>";
+		echo "<form name='addButton' action='addtocart($productID)' method='POST'><input type='submit' name='addToC' value='Add to Cart'></input></form>";
 		echo "</div>"; # Ending the container
 	}
 	#echo (mysql_num_rows($query) !== 0) ? mysql_result($query, 0, "name") : "Not found";
