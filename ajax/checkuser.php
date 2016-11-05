a1f5facb2b7287b96c5036c074f193ee83718bab
Lachlan Blackhall <vorcane@gmail.com>
2016-11-05 20:46:53 +0800

    test

--- ajax/checkuser.php
+++ ajax/checkuser.php
@@ -1,7 +1,7 @@
 <?php    
     require "../database/connect.php";
     $username = $_POST['username'];
-    $query = mysql_query("SELECT * FROM users WHERE username = '$username';")
+    $query = mysql_query("SELECT * FROM users WHERE username = '$username';");
     if (mysql_num_rows($query) !== 0){
         echo "1"; //Username taken
     } else {
