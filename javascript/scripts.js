40a00f0b0fdadf21ae4701b322b671fed26deadd
Lachlan Blackhall <vorcane@gmail.com>
2016-11-06 23:19:49 +0800

    test

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -284,8 +284,8 @@ function submitorder() {
             $('div#login-data').append(" products " + i + "<br>");
             //$('div#login-data').text(product);
         });
-        $('section#mainCont').append(products[0] + "<br>");
-        $('section#mainCont').append(products[1] + "<br>");
+        $('section#mainCont').append("I tryed to put p1 here " + products[0] + "<br>");
+        $('section#mainCont').append("I tried to put p2 here " + products[1] + "<br>");
         $('section#mainCont').append("<button type='button' id='submitbutton'>Submit Order</button>");
         $('section#mainCont').append("</div>");
         $('button#submitbutton').on('click', function () {
