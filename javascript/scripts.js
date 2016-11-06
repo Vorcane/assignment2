ef9c4a83a9c6a5eb3b8aa2d60331d9b453f908a2
Lachlan Blackhall <vorcane@gmail.com>
2016-11-06 23:02:57 +0800

    test

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -271,10 +271,9 @@ function submitorder() {
             $.post('ajax/cart.php', {productID: productID}, function (data) {
                 tProduct = data;
                 products[i] = tProduct + "<br>";
-                $('div#login-data').html(splitcart.length);
             });
         });
-        
+        $('div#login-data').html(products[0] + "and" + products[1]);
         $('section#mainCont').html("<div class = 'center'> You have chosen the following products: ");
         //$('div#login-form').html(products.length);
         products.forEach(function (product, i) {
