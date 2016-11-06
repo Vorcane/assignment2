2c0c6700d6a0232d6f71921b973cf43bae37a776
Lachlan Blackhall <vorcane@gmail.com>
2016-11-07 00:21:15 +0800

    test

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,6 +1,173 @@
 /*global $, Cookies*/
 /*jslint plusplus: true*/
 
+function dobuttons() {
+    "use strict";
+    $("a#fps").on("click", function () {
+        var search = $("a#fps").text();
+
+        if ($.trim(search) !== "") {
+            $.post("ajax/genre.php", {search: search}, function (data) {
+                $("div#search-data").html(data);
+            });
+        }
+    });
+
+    //Function to update page to only show MMO genre
+    $("a#mmo").on("click", function () {
+        var search = $("a#mmo").text();
+
+        if ($.trim(search) !== "") {
+            $.post("ajax/genre.php", {search: search}, function (data) {
+                $("div#search-data").html(data);
+            });
+        }
+    });
+
+    //Function to update page to only show RACING genre
+    $("a#racing").on("click", function () {
+        var search = $("a#racing").text();
+
+        if ($.trim(search) !== "") {
+            $.post("ajax/genre.php", {search: search}, function (data) {
+                $("div#search-data").html(data);
+            });
+        }
+    });
+
+    //Function to update page to only show ADVENTURE genre
+    $("a#adventure").on("click", function () {
+        var search = $("a#adventure").text();
+
+        if ($.trim(search) !== "") {
+            $.post("ajax/genre.php", {search: search}, function (data) {
+                $("div#search-data").html(data);
+            });
+        }
+    });
+
+    //Function to update page to only show sandbox genre
+    $("a#sandbox").on("click", function () {
+        var search = $("a#sandbox").text();
+
+        if ($.trim(search) !== "") {
+            $.post("ajax/genre.php", {search: search}, function (data) {
+                $("div#search-data").html(data);
+            });
+        }
+    });
+
+    //Function to update page to only show sports genre
+    $("a#sports").on("click", function () {
+        var search = $("a#sports").text();
+
+        if ($.trim(search) !== "") {
+            $.post("ajax/genre.php", {search: search}, function (data) {
+                $("div#search-data").html(data);
+            });
+        }
+    });
+
+    //Function to update page to only show action genre
+    $("a#action").on("click", function () {
+        var search = $("a#action").text();
+
+        if ($.trim(search) !== "") {
+            $.post("ajax/genre.php", {search: search}, function (data) {
+                $("div#search-data").html(data);
+            });
+        }
+    });
+
+    //Function to update page to only show rts genre
+    $("a#rts").on("click", function () {
+        var search = $("a#rts").text();
+
+        if ($.trim(search) !== "") {
+            $.post("ajax/genre.php", {search: search}, function (data) {
+                $("div#search-data").html(data);
+            });
+        }
+    });
+
+    //Function to update page to only show fighter genre
+    $("a#fighter").on("click", function () {
+        var search = $("a#fighter").text();
+
+        if ($.trim(search) !== "") {
+            $.post("ajax/genre.php", {search: search}, function (data) {
+                $("div#search-data").html(data);
+            });
+        }
+    });
+
+    //Function to update page to only show rpg genre
+    $("a#rpg").on("click", function () {
+        var search = $("a#rpg").text();
+
+        if ($.trim(search) !== "") {
+            $.post("ajax/genre.php", {search: search}, function (data) {
+                $("div#search-data").html(data);
+            });
+        }
+    });
+
+    // CODE FOR SEARCHING PRICE BELOW
+    // CODE FOR SEARCHING PRICE BELOW
+    // CODE FOR SEARCHING PRICE BELOW
+    // CODE FOR SEARCHING PRICE BELOW
+    // CODE FOR SEARCHING PRICE BELOW
+
+    //Function to update page to only show rpg genre
+    $("a#utwent").on("click", function () {
+        var search = parseFloat("20");
+
+        if ($.trim(search) !== "") {
+            $.post("ajax/price.php", {search: search}, function (data) {
+                $("div#search-data").html(data);
+            });
+        }
+    });
+
+    //Function to update page to only show rpg genre
+    $("a#ufifty").on("click", function () {
+        var search = parseFloat("50");
+
+        if ($.trim(search) !== "") {
+            $.post("ajax/price.php", {search: search}, function (data) {
+                $("div#search-data").html(data);
+            });
+        }
+    });
+
+    //Function to update page to only show rpg genre
+    $("a#ofifty").on("click", function () {
+        var search = parseFloat("52");
+
+        if ($.trim(search) !== "") {
+            $.post("ajax/price.php", {search: search}, function (data) {
+                $("div#search-data").html(data);
+            });
+        }
+    });
+
+
+    //ABOUT US
+    $("a#aboutus").click(function () {
+
+        $.ajax({
+            type: 'POST',
+            url: 'ajax/aboutus.php',
+            success: function (data) {
+                $("div#search-data").html(data);
+            }
+        });
+    });
+}
+
+
+
+
 
 function loadcart() {
     "use strict";
@@ -50,7 +217,7 @@ function checklogin() {
     "use strict";
     var sessionID = Cookies.get('sessionID'),
         outputhtml;
-    
+    dobuttons();
     $.post('ajax/checklogin.php', {sessionID: sessionID}, function (data) {
         var retdata = data.split(';');
         if (retdata[0] === "1") {
